cb89f237a4ea343bb2d40db5dd0fa7ffe5b9aade
Mark Bates <mark@mackframework.com>
2008-03-13 17:47:03 -0400

    Added distributed_route_test.rb file, with some basic tests. Currently these tests fail because, well, I haven't written the functionality behind them.

---
+++ test/routing/distributed_route_test.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../test_helper.rb'
+class DistributedRouteTest < Test::Unit::TestCase
+  
+  def test_unknown_application_droute_url
+    assert_raise(Mack::Errors::Distributed::UnknownApplication) { droute_url(:unknown_app, :foo_url) }
+  end
+  
+  def test_unknown_route_name_droute_url
+    assert_raise(Mack::Errors::Distributed::UnknownRouteName) { droute_url(:known_app, :unknown_url) }
+  end
+  
+  def test_droute_url
+    assert_equal "/my_known_app/my_known_url", droute_url(:known_app, :known_url)
+  end
+  
+  def test_droute_url_with_options
+    assert_equal "/my_known_app/my_known_url_w_opts/1", droute_url(:known_app, :known_url_w_opts, :id => 1)
+  end
+  
+end
