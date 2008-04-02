a8be6adae7f5c0fd3850630ffad5204049898cf3
Mark Bates <mark@mackframework.com>
2008-04-02 13:31:10 -0400

    Filters in controllers can now be inherited from parent controller classes.

--- tasks/gem_helper.rb
+++ tasks/gem_helper.rb
@@ -10,7 +10,7 @@ class GemHelper # :nodoc:
     self.project = "magrathea"
     self.package = "mack"
     self.gem_name = "mack"
-    self.version = "0.4.1"
+    self.version = "0.4.1.113"
   end
   
   def gem_name_with_version
--- test/sea_level/filter_test.rb
+++ test/sea_level/filter_test.rb
@@ -3,26 +3,42 @@ require File.dirname(__FILE__) + '/../test_helper.rb'
 class FilterTest < Test::Unit::TestCase
   
   def test_run
-    f = Mack::Controller::Filter.new(:log_action)
+    f = Mack::Controller::Filter.new(:log_action, self)
     assert f.run?(:my_action)
     
-    f = Mack::Controller::Filter.new(:log_action, :only => :my_action)
+    f = Mack::Controller::Filter.new(:log_action, self, :only => :my_action)
     assert f.run?(:my_action)
     assert !f.run?(:my_other_action)
     
-    f = Mack::Controller::Filter.new(:log_action, :except => :my_action)
+    f = Mack::Controller::Filter.new(:log_action, self, :except => :my_action)
     assert !f.run?(:my_action)
     assert f.run?(:my_other_action)
     
-    f = Mack::Controller::Filter.new(:log_action, :only => [:my_action, :my_other_action])
+    f = Mack::Controller::Filter.new(:log_action, self, :only => [:my_action, :my_other_action])
     assert f.run?(:my_action)
     assert f.run?(:my_other_action)
     assert !f.run?(:some_other_action)
     
-    f = Mack::Controller::Filter.new(:log_action, :except => [:my_action, :my_other_action])
+    f = Mack::Controller::Filter.new(:log_action, self, :except => [:my_action, :my_other_action])
     assert !f.run?(:my_action)
     assert !f.run?(:my_other_action)
     assert f.run?(:some_other_action)
+  end
+  
+  def test_to_s
+    f = Mack::Controller::Filter.new(:log_action, FilterTest)
+    puts f
+    assert_equal "FilterTest.log_action", f.to_s
+  end
+  
+  def test_equals
+    assert_equal Mack::Controller::Filter.new(:log_action, FilterTest), Mack::Controller::Filter.new(:log_action, FilterTest)
+  end
+  
+  def test_array_uniq
+    fs = [Mack::Controller::Filter.new(:log_action, FilterTest), Mack::Controller::Filter.new(:log_action, FilterTest)]
+    fs.uniq!
+    assert_equal [Mack::Controller::Filter.new(:log_action, FilterTest)], fs
   end
   
   def test_before_all_actions
