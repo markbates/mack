375adc48e254ca1e870031500bdfc453c94e8f15
Mark Bates <mark@mackframework.com>
2008-04-06 22:36:08 -0400

    More DataMapper migration work.

--- test/fake_application/app/models/car.rb
+++ test/fake_application/app/models/car.rb
@@ -1,2 +1,5 @@
 class Car < DataMapper::Base
+  property :name, :string, :nullable => false
+  property :color, :string, :nullable => false
+  property :wheel_count, :integer, :default => 4
 end
--- test/fake_application/app/models/tree.rb
+++ test/fake_application/app/models/tree.rb
@@ -1,2 +1,4 @@
 class Tree < DataMapper::Base
+  property :leaf_color, :string, :nullable => false
+  property :height, :decimal, :nullable => false
 end
