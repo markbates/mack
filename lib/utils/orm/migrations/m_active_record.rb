df7eb2e647f1cf7dcbfe5d29e423a62e3726650b
Mark Bates <mark@mackframework.com>
2008-04-09 17:53:44 -0400

    More work on migrations.

---
+++ lib/utils/orm/migrations/m_active_record.rb
@@ -0,0 +1,10 @@
+module Mack
+  module Utils
+    module Orm
+      module Migrations
+        class ActiveRecord < Mack::Utils::Orm::Migrations::Base
+        end # ActiveRecord
+      end # Migrations
+    end # Orm
+  end # Utils
+end # Mack
---
+++ test/lib/utils/orm/migrations/migration_base_utils_test.rb
@@ -0,0 +1,5 @@
+require File.dirname(__FILE__) + '/../../../../test_helper.rb'
+
+class MigrationBaseUtilsTest < Test::Unit::TestCase
+  
+end
