4561c0f02b658f438c66f51e3b4cd8fca92af802
Mark Bates <mark@mackframework.com>
2008-04-09 13:15:14 -0400

    More testing of generators.

--- test/lib/generators/base_generator_test.rb
+++ test/lib/generators/base_generator_test.rb
@@ -2,12 +2,55 @@ require File.dirname(__FILE__) + '/../../test_helper.rb'
 
 class BaseGeneratorTest < Test::Unit::TestCase
   
-  def setup
+  class SimpleGenerator < Mack::Generator::Base
+    require_param :name
+    require_param :id
+    
+    def generate
+      directory(File.join(MACK_ROOT, "test", "simple_gen"))
+    end
     
   end
   
+  class BadGenerator < Mack::Generator::Base
+  end
+  
+  def setup
+    if File.exists?(File.join(MACK_ROOT, "test", "simple_gen"))
+      FileUtils.rm_r(File.join(MACK_ROOT, "test", "simple_gen"))
+    end
+  end
+  
   def teardown
-    
+    if File.exists?(File.join(MACK_ROOT, "test", "simple_gen"))
+      FileUtils.rm_r(File.join(MACK_ROOT, "test", "simple_gen"))
+    end
+  end
+  
+  def test_require_param
+    assert_raise(Mack::Errors::RequiredGeneratorParameterMissing) { SimpleGenerator.new }
+    assert_raise(Mack::Errors::RequiredGeneratorParameterMissing) { SimpleGenerator.new("name" => :foo) }
+    assert_raise(Mack::Errors::RequiredGeneratorParameterMissing) { SimpleGenerator.new("id" => 1) }
+    sg = SimpleGenerator.new("name" => :foo, "id" => 1)
+    assert_not_nil sg
+    assert_equal 1, sg.param(:id)
+    assert_equal :foo, sg.param(:name)
+  end
+  
+  def test_run
+    bg = BadGenerator.new
+    assert_raise(MethodNotImplemented) { bg.run }
+    assert_raise(MethodNotImplemented) { bg.generate }
+  end
+  
+  def test_directory
+    assert !File.exists?(File.join(MACK_ROOT, "test", "simple_gen"))
+    sg = SimpleGenerator.new("name" => :foo, "id" => 1)
+    sg.run
+    assert File.exists?(File.join(MACK_ROOT, "test", "simple_gen"))
+    # run it again to prove we don't get any errors if the folder already exists
+    sg.run
+    assert File.exists?(File.join(MACK_ROOT, "test", "simple_gen"))
   end
   
 end
