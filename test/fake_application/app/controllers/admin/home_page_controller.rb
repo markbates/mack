c7d3aa6f76a9206b4ca2230c98971bcc481194f0
dsutedja <dsutedja@helium.com>
2008-04-23 10:39:19 -0400

    oops, missed these 2 test files

---
+++ test/fake_application/app/controllers/admin/home_page_controller.rb
@@ -0,0 +1,26 @@
+module Admin
+  
+  class HomePageController < Mack::Controller::Base
+    def index
+    end
+    
+    def create
+    end
+    
+    def new
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def delete
+    end
+  end
+  
+end
