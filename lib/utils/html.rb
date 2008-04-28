95045ec8988e89d0e7cefa6c1bf1c9ce2d49cf9d
Mark Bates <mark@mackframework.com>
2008-04-28 14:49:42 -0400

    RDoc for html stuff.

--- lib/utils/html.rb
+++ lib/utils/html.rb
@@ -9,17 +9,17 @@ module Mack
         # to the href as options.
         # 
         # Examples:
-        #    Mack::Utils::Html.href("http://www.mackframework.com") # => <a href="http://www.mackframework.com">http://www.mackframework.com</a>
-        #    Mack::Utils::Html.href("Mack", "http://www.mackframework.com") # => <a href="http://www.mackframework.com">Mack</a>
-        #    Mack::Utils::Html.href("Mack", "http://www.mackframework.com", :target => "_blank") # => <a href="http://www.mackframework.com" target="_blank">Mack</a>
-        #    Mack::Utils::Html.href("Mack", "http://www.mackframework.com", :target => "_blank", :rel => :nofollow) # => <a href="http://www.mackframework.com" target="_blank" rel="nofollow">Mack</a>
+        #    Mack::Utils::Html.a("http://www.mackframework.com") # => <a href="http://www.mackframework.com">http://www.mackframework.com</a>
+        #    Mack::Utils::Html.a("Mack", :href => "http://www.mackframework.com") # => <a href="http://www.mackframework.com">Mack</a>
+        #    Mack::Utils::Html.a("Mack", :href => "http://www.mackframework.com", :target => "_blank") # => <a href="http://www.mackframework.com" target="_blank">Mack</a>
+        #    Mack::Utils::Html.a("Mack", :href => "http://www.mackframework.com", :target => "_blank", :rel => :nofollow) # => <a href="http://www.mackframework.com" target="_blank" rel="nofollow">Mack</a>
         # If you pass in :method as an option it will be a JavaScript form that will post to the specified link with the 
         # methd specified.
-        #    Mack::Utils::Html.href("Mack", "http://www.mackframework.com", :method => :delete)
+        #    Mack::Utils::Html.a("Mack", :href => "http://www.mackframework.com", :method => :delete)
         # If you use the :method option you can also pass in a :confirm option. The :confirm option will generate a 
         # javascript confirmation window. If 'OK' is selected the the form will submit. If 'cancel' is selected, then
         # nothing will happen. This is extremely useful for 'delete' type of links.
-        #    Mack::Utils::Html.href("Mack", "http://www.mackframework.com", :method => :delete, :confirm => "Are you sure?")
+        #    Mack::Utils::Html.a("Mack", :href => "http://www.mackframework.com", :method => :delete, :confirm => "Are you sure?")
         def a(link_text, options = {})
           options = {:href => link_text}.merge(options)
           if options[:method]
@@ -37,13 +37,6 @@ module Mack
             options[:onclick] = (confirm ? (confirm + " { ") : "") << meth << (confirm ? (" } ") : "") << ";return false;"
           end
           content_tag(:a, link_text, options)
-          # 
-          # html = "<a href=" << '"' << url
-          # html << '"'
-          # html << " " << html_options.join("%s=\"%s\"", " ") unless html_options.empty?
-          # html << ">" << link_text
-          # html << "</a>"
-          # html
         end
         
         alias_method :href, :a
@@ -96,6 +89,11 @@ module Mack
           "<#{tag}#{build_options(options)}>#{content}</#{tag}>"
         end
         
+        # Builds an HTML tag with no content.
+        # 
+        # Examples:
+        #   non_content_tag(:br) # => <br />
+        #   non_content_tag(:hr, :width => "100%") # => <hr width="100%" />
         def non_content_tag(tag, options = {})
           "<#{tag}#{build_options(options)} />"
         end
