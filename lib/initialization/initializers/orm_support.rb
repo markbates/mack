2a6cf40710152ce235406bb78f0244a7079d639f
Mark Bates <mark@mackframework.com>
2008-05-02 16:46:31 -0400

    Added symbolize_keys to database config settings.

--- lib/initialization/initializers/orm_support.rb
+++ lib/initialization/initializers/orm_support.rb
@@ -1,16 +1,18 @@
 #--
 # setup ORM:
 #++
-
+gem "data_objects", "0.2.0"
+gem 'datamapper', "0.3.2"
 require 'data_mapper'
 
 dbs = YAML::load(Erubis::Eruby.new(IO.read(File.join(MACK_CONFIG, "database.yml"))).result)
 
 unless dbs.nil?
   settings = dbs[MACK_ENV]
-  if settings["default"]
+  settings.symbolize_keys!
+  if settings[:default]
     settings.each do |k,v|
-      DataMapper::Database.setup(k.to_sym, v)
+      DataMapper::Database.setup(k, v.symbolize_keys)
     end
   else
     DataMapper::Database.setup(settings)
