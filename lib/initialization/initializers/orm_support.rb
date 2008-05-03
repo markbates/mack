6c5dd251580b9892532c6d16acf5b6fbfd4ea929
Mark Bates <mark@mackframework.com>
2008-05-03 16:00:51 -0400

    Working on splitting out the ORM support into different gems.

--- lib/initialization/initializers/orm_support.rb
+++ lib/initialization/initializers/orm_support.rb
@@ -1,27 +1,28 @@
 #--
 # setup ORM:
 #++
-gem "data_objects", "0.2.0"
-gem 'datamapper', "0.3.2"
-require 'data_mapper'
-
-dbs = YAML::load(Erubis::Eruby.new(IO.read(File.join(MACK_CONFIG, "database.yml"))).result)
-
-unless dbs.nil?
-  settings = dbs[MACK_ENV]
-  settings.symbolize_keys!
-  if settings[:default]
-    settings.each do |k,v|
-      DataMapper::Database.setup(k, v.symbolize_keys)
-    end
-  else
-    DataMapper::Database.setup(settings)
-  end
-end
-
-class DmSchemaInfo # :nodoc:
-  include DataMapper::Persistence
-  
-  set_table_name "schema_info"
-  property :version, :integer, :default => 0
-end
+require 'mack-data_mapper'
+# gem "data_objects", "0.2.0"
+# gem 'datamapper', "0.3.2"
+# require 'data_mapper'
+# 
+# dbs = YAML::load(Erubis::Eruby.new(IO.read(File.join(MACK_CONFIG, "database.yml"))).result)
+# 
+# unless dbs.nil?
+#   settings = dbs[MACK_ENV]
+#   settings.symbolize_keys!
+#   if settings[:default]
+#     settings.each do |k,v|
+#       DataMapper::Database.setup(k, v.symbolize_keys)
+#     end
+#   else
+#     DataMapper::Database.setup(settings)
+#   end
+# end
+# 
+# class DmSchemaInfo # :nodoc:
+#   include DataMapper::Persistence
+#   
+#   set_table_name "schema_info"
+#   property :version, :integer, :default => 0
+# end
