8f7b38ad0373835539beffeb4c8408ca554f1ca0
Mark Bates <mark@mackframework.com>
2008-05-07 12:53:11 -0400

    gem: mack_ruby_core_extensions 0.1.25

--- test/fake_application/config/routes.rb
+++ test/fake_application/config/routes.rb
@@ -1,24 +1,30 @@
 Mack::Routes.build do |r|
 
   r.resource "admin/users"
+  
+  r.with_options(:controller => :tst_home_page) do |map|
+    map.connect "/"
+    map.connect "foo", :action => :foo
+    map.connect "hello_from_render_text", :action => :hello_from_render_text
+    map.connect "/foo_from_render_action", :action => :foo_from_render_action
+    map.connect "/blow_from_bad_render_action", :action => :blow_from_bad_render_action
+    map.connect "blow_up_from_double_render", :action => :blow_up_from_double_render
+    map.connect '/sess_test', :action => :read_param_from_session
+    map.hello_world "/hello/world", :action => :hello_world
+  end
+  
+  r.with_options(:controller => :tst_another) do |map|
+    map.pickles "/pickles", :action => :foo, :method => :post
+    map.env '/env', :action => :env
+    map.kill_kenny_no_meth "/tst_another/kill_kenny", :action => :kill_kenny
+    map.kill_kenny_bad "/tst_another/kill_kenny_bad", :action => :kill_kenny_bad
+  end
+  
 
-  r.connect "/", :controller => :tst_home_page
-  r.connect "foo", :controller => :tst_home_page, :action => :foo
-  r.pickles "/pickles", :controller => :tst_another, :action => :foo, :method => :post
-  r.connect "hello_from_render_text", :controller => :tst_home_page, :action => :hello_from_render_text
-  r.connect "/foo_from_render_action", :controller => :tst_home_page, :action => :foo_from_render_action
-  r.connect "/blow_from_bad_render_action", :controller => :tst_home_page, :action => :blow_from_bad_render_action
-  r.connect "blow_up_from_double_render", :controller => :tst_home_page, :action => :blow_up_from_double_render
-  r.connect '/sess_test', :controller => :tst_home_page, :action => :read_param_from_session
-  r.env '/env', :controller => :tst_another, :action => :env
-                                                                        
-  r.hello_world "/hello/world", :controller => :tst_home_page, :action => :hello_world
   
   r.old_foo "/my_old_foo", :redirect_to => "/tst_another/foo/:id", :status => 301
   
   r.kill_kenny_good "/tst_users/kill_kenny_good", :controller => :tst_users, :action => :kill_kenny_good
-  r.kill_kenny_no_meth "/tst_another/kill_kenny", :controller => :tst_another, :action => :kill_kenny
-  r.kill_kenny_bad "/tst_another/kill_kenny_bad", :controller => :tst_another, :action => :kill_kenny_bad
   r.resource :tst_users
   r.resource :tst_resources
   
