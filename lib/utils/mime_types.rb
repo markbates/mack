ddc43d1cf4ee0c605e8e12c91c575343039e65ee
Mark Bates <mark@mackframework.com>
2008-05-07 15:09:32 -0400

    Added RDoc for Mack::Utils::MimeTypes

--- lib/utils/mime_types.rb
+++ lib/utils/mime_types.rb
@@ -1,5 +1,6 @@
 module Mack
   module Utils
+    # Houses the MimeTypes used by the system to set the proper 'Content-Type' for requests.
     class MimeTypes
       include Singleton
       
@@ -7,10 +8,14 @@ module Mack
         @types = YAML.load(File.open(File.join(File.dirname(__FILE__), "mime_types.yml")))
       end
       
-      # Registers a new mime-type to the system.
+      # Registers a new mime-type to the system. If the key already exists, it will
+      # be appended to the existing type.
       # 
       # Examples:
       # Mack::Utils::MimeTypes.register(:iphone, "app/iphone")
+      #   "/users.iphone" # => will have a 'Content-Type' header of "app/iphone"
+      # Mack::Utils::MimeTypes.register(:iphone, "application/mac-iphone")
+      #   "/users.iphone" # => will have a 'Content-Type' header of "app/iphone; application/mac-iphone"
       def register(name, type)
         name = name.to_sym
         if @types.has_key?(name)
@@ -20,16 +25,26 @@ module Mack
         end
       end
       
+      # Returns the type registered for the key, if there is no type for the key,
+      # then "text/html" is returned
+      # 
+      # Examples:
+      # Mack::Utils::MimeTypes.get(:iphone)
+      #   # => "app/iphone"
+      # Mack::Utils::MimeTypes.get(:i_dont_exist)
+      #   # => "text/html"
       def get(name)
         @types[name.to_sym] || "text/html"
       end
       
       class << self
         
+        # Maps to Mack::Utils::MimeTypes.instance.get
         def [](name)
           Mack::Utils::MimeTypes.instance.get(name.to_sym)
         end
         
+        # Maps to Mack::Utils::MimeTypes.instance.register
         def register(name, type)
           Mack::Utils::MimeTypes.instance.register(name, type)
         end
