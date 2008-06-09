7d5a5abc9bf752e915a6918421bc0d5cf0212016
Mark Bates <mark@mackframework.com>
2008-06-08 22:26:34 -0400

    Forgot to update the gem version 0.5.5.4

--- tasks/gem_helper.rb
+++ tasks/gem_helper.rb
@@ -10,7 +10,7 @@ class GemHelper # :nodoc:
     self.project = "magrathea"
     self.package = "mack"
     self.gem_name = "mack"
-    self.version = "0.5.5.3"
+    self.version = "0.5.5.4"
   end
   
   def gem_name_with_version
