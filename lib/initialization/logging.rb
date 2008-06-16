96434323946ea5ab17150a1ab5c573bd717a3cbf
Mark Bates <mark@markbates.com>
2008-06-16 14:53:33 -0400

    Added a logger= method.

--- lib/initialization/logging.rb
+++ lib/initialization/logging.rb
@@ -8,24 +8,26 @@ module Mack
     $mack_default_logger
   end
   
+  def self.logger=(log)
+    $mack_default_logger = log
+  end
+  
 end
 
 unless Mack.logger
   log_directory = app_config.log_root || File.join(Mack::Configuration.root, "log")
   FileUtils.mkdir_p(log_directory)
 
-  log = Log4r::Logger.new('')
-  log.level =  Module.instance_eval("Log4r::#{(app_config.log_level || :info).to_s.upcase}")
+  Mack.logger = Log4r::Logger.new('')
+  Mack.logger.level =  Module.instance_eval("Log4r::#{(app_config.log_level || :info).to_s.upcase}")
   
   format = Log4r::PatternFormatter.new(:pattern => "%l:\t[%d]\t%M")
   
   if Mack::Configuration.env == "development"
     # console:
-    log.add(Log4r::StdoutOutputter.new('console', :formatter => format))
+    Mack.logger.add(Log4r::StdoutOutputter.new('console', :formatter => format))
   end
   
   # file:
-  log.add(Log4r::FileOutputter.new('fileOutputter', :filename => File.join(log_directory, "#{Mack::Configuration.env}.log"), :trunc => false, :formatter => format))
-  
-  $mack_default_logger = log
+  Mack.logger.add(Log4r::FileOutputter.new('fileOutputter', :filename => File.join(log_directory, "#{Mack::Configuration.env}.log"), :trunc => false, :formatter => format))
 end
