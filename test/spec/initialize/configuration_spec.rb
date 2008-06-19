b39c31a9fa603dca51ccb99583ae3b94120c34e6
dsutedja <dsutedja@helium.com>
2008-06-19 15:31:29 -0400

    - converted test/initialize to rspec

---
+++ test/spec/initialize/configuration_spec.rb
@@ -0,0 +1,24 @@
+require 'pathname'
+require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
+
+describe "Configuration" do
+  
+  describe "=> Existence Check" do
+    it "should have valid reference to app_config" do
+      app_config.should_not be_nil
+    end
+    
+    it "should have valid reference to mack's app_config" do
+      app_config.mack.should_not be_nil
+    end
+    
+    it "should return nil if unknown config key is requested" do
+      app_config.mack.foo_bar
+    end
+    
+    it "should return valid reference if requested config key is valid" do
+      app_config.mack.cache_classes.should == true
+    end
+  end
+  
+end
