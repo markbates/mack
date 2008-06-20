cc25d9647dcd1a30b539322f23d2e53dbedb3016
dsutedja <dsutedja@helium.com>
2008-06-20 19:20:20 -0400

    - converted plugin_generator_test to spec test

---
+++ test/spec/lib/utils/generators/plugin_generator_spec.rb
@@ -0,0 +1,38 @@
+require 'pathname'
+require Pathname(__FILE__).dirname.expand_path.parent.parent.parent + 'spec_helper'
+
+describe "Plugin Generator" do
+  
+  after(:all) do
+    clean_bandit_dir  
+  end
+  
+  it "should be able to properly generate the plugin files" do 
+    pg = PluginGenerator.new("name" => "bandit")
+    File.exists?(bandit_dir).should_not == true
+    File.exists?(File.join(bandit_dir, "init.rb")).should_not == true
+    File.exists?(File.join(bandit_dir, "lib")).should_not == true
+    File.exists?(File.join(bandit_dir, "lib", "bandit.rb")).should_not == true
+    File.exists?(File.join(bandit_dir, "lib", "tasks")).should_not == true
+    
+    pg.generate
+    File.exists?(bandit_dir).should == true
+    File.exists?(File.join(bandit_dir, "init.rb")).should == true
+    File.exists?(File.join(bandit_dir, "lib")).should == true
+    File.exists?(File.join(bandit_dir, "lib", "bandit.rb")).should == true
+    File.exists?(File.join(bandit_dir, "lib", "tasks")).should == true
+    File.exists?(File.join(bandit_dir, "lib", "tasks", "bandit_tasks.rake")).should == true
+    
+    File.open(File.join(bandit_dir, "init.rb"), "r") do |f|
+      f.read.should == "require 'bandit'\n"
+    end
+
+    File.open(File.join(bandit_dir, "lib", "bandit.rb"), "r") do |f|
+      f.read.should == "# Do work here for bandit\n"
+    end
+  end
+  
+  def bandit_dir
+    File.join(Mack::Configuration.plugins, "bandit")
+  end
+end
