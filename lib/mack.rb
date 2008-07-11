79bc27de4a80bb752a1fbc3cc388088905cd86ea
Mark Bates <mark@markbates.com>
2008-07-11 11:25:11 -0400

    Split initialization of helpers into its own file.

--- lib/mack.rb
+++ lib/mack.rb
@@ -47,35 +47,9 @@ unless Mack::Configuration.initialized
   
   require File.join(File.dirname(__FILE__), "initialization", "orm_support.rb")
 
-  # ------------------------------------------------------------------------
-
-  # set up application stuff:
-
   require File.join(File.dirname(__FILE__), "initialization", "application.rb")
   
-
-  # ------------------------------------------------------------------------ 
-  
-  # Include ApplicationHelper into all controllers:
-  Mack.logger.info "Initializing helpers..."
-  # adding application_helper module into all defined controllers
-  Object.constants.collect {|c| c if c.match(/Controller$/)}.compact.each do |cont|
-    ApplicationHelper.include_safely_into(cont, Mack::Rendering::ViewTemplate)
-  end
-  
-  # Find other Helpers and include them into their respective controllers.
-  Object.constants.collect {|c| c if c.match(/Controller$/)}.compact.each do |cont|
-    if Object.const_defined?("#{cont}Helper")
-      h = "#{cont}Helper".constantize
-      h.include_safely_into(cont, Mack::Rendering::ViewTemplate)
-    end
-  end
-  
-  # Find view level Helpers and include them into the Mack::Rendering::ViewTemplate
-  Mack::ViewHelpers.constants.each do |cont|
-      h = "Mack::ViewHelpers::#{cont}".constantize
-      h.include_safely_into(Mack::Rendering::ViewTemplate)
-  end
+  require File.join(File.dirname(__FILE__), "initialization", "helpers.rb")
   
   Mack::Configuration.initialized = true if Mack::Configuration.initialized.nil?
 
