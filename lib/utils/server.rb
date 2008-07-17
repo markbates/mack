f5999ffcf81cd51bcd2ad4feba5aeb0f5542a630
Mark Bates <mark@markbates.com>
2008-07-17 18:08:12 -0400

    Adding the beginnings of hooks for page caching and other Rack 'wrappers'

--- lib/utils/server.rb
+++ lib/utils/server.rb
@@ -7,6 +7,12 @@ module Mack
       def self.build_app
         # Mack framework:
         app = Mack::Runner.new
+        
+        Mack::Utils::Server::Registry.instance.wrappers do |w|
+          puts "Wrapping app with: #{w}"
+          app = w.new(app)
+        end
+        
         # Any urls listed will go straight to the public directly and will not be served up via the app:
         app = Rack::Static.new(app, :urls => ["/css", "/images", "/files", "/images", "/stylesheets", "/javascripts", "/media"], :root => "public")
         app = Rack::Lint.new(app) if app_config.mack.use_lint 
@@ -20,6 +26,21 @@ module Mack
         # This makes it a drag run this 'standalone' in another Ruby program.
         # app = Rack::CommonLogger.new(app, Mack.logger)
         app
+      end
+      
+      class Registry
+        include Singleton
+        
+        attr_reader :wrappers
+        
+        def initialize
+          @wrappers = []
+        end
+        
+        def add(klass)
+          @wrappers << klass
+        end
+        
       end
     
     end # Server
