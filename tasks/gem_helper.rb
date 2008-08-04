cb50a587234dcc31dcbf8f4556ea291f65e232b4
Mark Bates <mark@markbates.com>
2008-08-04 11:23:39 -0400

    Release 0.6.1

--- tasks/gem_helper.rb
+++ tasks/gem_helper.rb
@@ -10,7 +10,7 @@ class GemHelper # :nodoc:
     self.project = "magrathea"
     self.package = "mack"
     self.gem_name = "mack"
-    self.version = "0.6.0.100"
+    self.version = "0.6.1"
   end
   
   def gem_name_with_version
