84de40b7f7dcd83a7705d0d24ca5482e40291efa
Mark Bates <mark@mackframework.com>
2008-08-08 22:50:31 -0400

    Added a reset_logger! method.

--- lib/mack/initialization/logging.rb
+++ lib/mack/initialization/logging.rb
@@ -12,10 +12,27 @@ module Mack
     $mack_default_logger = log
   end
   
+  def self.reset_logger!
+    log_directory = app_config.log_root || File.join(Mack.root, "log")
+    FileUtils.mkdir_p(log_directory)
+
+    Mack.logger = Log4r::Logger.new('')
+    Mack.logger.level =  Module.instance_eval("Log4r::#{(app_config.log_level || :info).to_s.upcase}")
+
+    format = Log4r::PatternFormatter.new(:pattern => "%l:\t[%d]\t%M")
+
+    if Mack.env == "development"
+      # console:
+      Mack.logger.add(Log4r::StdoutOutputter.new('console', :formatter => format))
+    end
+
+    # file:
+    Mack.logger.add(Log4r::FileOutputter.new('fileOutputter', :filename => File.join(log_directory, "#{Mack.env}.log"), :trunc => false, :formatter => format))    
+  end
+  
 end
 
 unless Mack.logger
-
   module Log4r # :nodoc:
     class IOOutputter # :nodoc:
 
@@ -40,21 +57,7 @@ unless Mack.logger
     end # IOOutputter
   end # Log4r
   
-  log_directory = app_config.log_root || File.join(Mack.root, "log")
-  FileUtils.mkdir_p(log_directory)
-
-  Mack.logger = Log4r::Logger.new('')
-  Mack.logger.level =  Module.instance_eval("Log4r::#{(app_config.log_level || :info).to_s.upcase}")
-  
-  format = Log4r::PatternFormatter.new(:pattern => "%l:\t[%d]\t%M")
-  
-  if Mack.env == "development"
-    # console:
-    Mack.logger.add(Log4r::StdoutOutputter.new('console', :formatter => format))
-  end
-  
-  # file:
-  Mack.logger.add(Log4r::FileOutputter.new('fileOutputter', :filename => File.join(log_directory, "#{Mack.env}.log"), :trunc => false, :formatter => format))
+  Mack.reset_logger!
 end
 
 module Mack
