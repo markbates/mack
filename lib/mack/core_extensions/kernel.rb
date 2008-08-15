650cf562ef808648221a2029f415c9f0e8329f30
Mark Bates <mark@markbates.com>
2008-08-15 11:18:47 -0400

    Added deprecation system. [#90 state:resolved]

--- lib/mack/core_extensions/kernel.rb
+++ lib/mack/core_extensions/kernel.rb
@@ -5,8 +5,22 @@ module Kernel
     yield Mack::Utils::GemManager.instance
   end
   
-  def alias_deprecated_method(deprecated_method, new_method, version_deprecrated = nil, version_to_be_removed = nil) # :nodoc:
-    message = "DEPRECATED: '#{deprecated_method}'. Please use '#{new_method}' instead."
+  # Aliases the deprecated method to the new method and logs a warning.
+  def alias_deprecated_method(deprecated_method, new_method, version_deprecrated = nil, version_to_be_removed = nil)
+    eval %{
+      def #{deprecated_method}(*args)
+        deprecate_method(:#{deprecated_method}, :#{new_method}, "#{version_deprecrated}", "#{version_to_be_removed}")
+        #{new_method}(*args)
+      end
+    }
+  end
+  
+  # Logs a warning that a method has been deprecated. Warnings will only get logged once.
+  def deprecate_method(deprecated_method, new_method = nil, version_deprecrated = nil, version_to_be_removed = nil)
+    message = "DEPRECATED: '#{deprecated_method}'."
+    if new_method
+      message << " Please use '#{new_method}' instead."
+    end
     if version_deprecrated
       message << " Deprecated in version: '#{version_deprecrated}'."
       if version_to_be_removed.nil?
@@ -16,12 +30,14 @@ module Kernel
     if version_to_be_removed
       message << " To be removed in version: '#{version_to_be_removed}'."
     end
-    eval %{
-      def #{deprecated_method}(*args)
-        Mack.logger.warn("#{message}")
-        #{new_method}(*args)
-      end
-    }
+    unless Kernel::DeprecatedRegistryList.registered_items.include?(message)
+      Mack.logger.warn(message)
+      Kernel::DeprecatedRegistryList.add(message)
+    end
+  end
+  
+  private
+  class DeprecatedRegistryList < Mack::Utils::RegistryList # :nodoc:
   end
   
 end
