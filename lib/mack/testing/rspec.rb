a46eceb7316d86a802efe21e97339522cd70205f
Mark Bates <mark@markbates.com>
2008-08-18 11:18:08 -0400

    Fixed automatic in_session for testing.

--- lib/mack/testing/rspec.rb
+++ lib/mack/testing/rspec.rb
@@ -7,15 +7,23 @@ module Spec # :nodoc:
       include Mack::Routes::Urls
       include Mack::Testing::Helpers
     
-      alias_method :mack_spec_execute, :execute
+      # alias_method :mack_spec_execute, :execute
+      alias_method :mack_run_with_description_capturing, :run_with_description_capturing
 
-      def execute(options, instance_variables)
+      # def execute(options, instance_variables)
+      #   in_session do
+      #     @__res = mack_spec_execute(options, instance_variables)
+      #   end
+      #   @__res
+      # end
+      
+      def run_with_description_capturing
         in_session do
-          @__res = mack_spec_execute(options, instance_variables)
+          @__res_2 = mack_run_with_description_capturing
         end
-        @__res
+        @__res_2
       end
     
-    end
+    end 
   end
 end
