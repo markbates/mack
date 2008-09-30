d694a8df99e8bab92456f405a03b7947fa8d6e7a
dsutedja <dsutedja@helium.com>
2008-09-30 09:20:41 -0400

    rdoc for asset_mgr

--- lib/mack/view_helpers/assets_mgr.rb
+++ lib/mack/view_helpers/assets_mgr.rb
@@ -1,28 +1,58 @@
 module Mack
+  #
+  # Like the name suggests, this object will manage
+  # assets (i.e. scripts and stylesheets) used in a mack application.
+  # Developer will be able to use the manager to create
+  # assets groups, and refer them later in the erb file.
+  # <br>
+  # There's a convenient method available, called assets_mgr.
+  # Application should use that method to create new bundle, and to access
+  # defined bundles.
+  # <br>
+  # Example:
+  #   # This will create new bundle called foo
+  #   assets_mgr.foo do |a|
+  #     a.add_js "foo"
+  #     a.add_css "bar"
+  #   end
+  #   
+  # Note that nested group is not supported.
+  #
   class AssetsManager
     include Singleton
   
-    def initialize
+    def initialize # :nodoc:
       @group_hash = {}
       @javascripts = nil
       @stylesheets = nil
     end
     
+    #
+    # Clear the defined assets
+    #
     def reset!
       @group_hash.clear
     end
   
-    def method_missing(sym, *args, &block)
+    def method_missing(sym, *args, &block) # :nodoc:
       @group_hash[sym] ||= {:javascripts => [], :stylesheets => []}
       @javascripts = @group_hash[sym][:javascripts]
       @stylesheets = @group_hash[sym][:stylesheets]
       yield(self) if block_given?
     end
     
+    # 
+    # return all groups defined for both javascript and stylesheets
+    #
     def groups
       return @group_hash.keys
     end
     
+    # 
+    # Return all groups defined by specified asset type
+    #
+    # <i>Params:</i>
+    #   type -- asset type (can be either :javascript or :stylesheet)
     def groups_by_asset_type(type)
       arr = []
       groups.each do |group|
@@ -31,29 +61,44 @@ module Mack
       return arr
     end
     
-    def content_types
+    #
+    # Return supported asset types
+    #
+    def asset_types
       return [:javascripts, :stylesheets]
     end
   
+    #
+    # Get all javascript asset files defined for the specified group/bundle
+    #
     def javascripts(group)
       return nil if !@group_hash.has_key?(group.to_sym)
       return @group_hash[group.to_sym][:javascripts]
     end
-  
+    
+    #
+    # Get all stylesheet asset files defined for the specified group/bundle
+    #
     def stylesheets(group)
       return nil if !@group_hash.has_key?(group.to_sym)
       return @group_hash[group.to_sym][:stylesheets]
     end
       
+    #
+    # Add javascript file(s) to the group/bundle
+    #
     def add_js(data)
       return add_data(data, 'javascripts', '.js')
     end
   
+    #
+    # Add stylesheet file(s) to the group/bundle
+    #
     def add_css(data)
       return add_data(data, 'stylesheets', '.css')
     end
-  
-    def display
+
+    def display # :nodoc:
       pp @group_hash
     end
   
@@ -71,6 +116,9 @@ module Mack
 end
 
 module Kernel
+  #
+  # Return the instance of the AssetManager class.
+  #
   def assets_mgr
     return Mack::AssetsManager.instance
   end
